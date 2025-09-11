185526833f64a9140cbd20d6c2f29b2622a19e0f
Marina Barramus <marinabarramus@MacBook-Air-Marina.local>
2025-09-11 16:30:33 +0300

    Update landing

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,9 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/",
+  server: { host: "::", port: 8080 },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
+  build: { outDir: "dist" },
 }));
